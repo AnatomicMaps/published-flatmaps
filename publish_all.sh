6adc1950e93f8895cd6cacd9db1e154d04b110b5
David Brooks <d.brooks@auckland.ac.nz>
2022-12-12 14:23:55 +1300

    Allow extra parameters to be passed to `mapmaker` from publication scripts.

--- publish_all.sh
+++ publish_all.sh
@@ -2,9 +2,9 @@
 
 rm ./logs/*.log
 
-./publish.sh pig
-./publish.sh cat
-./publish.sh mouse
-./publish.sh rat
-./publish_human.sh female
-./publish_human.sh male
+./publish.sh pig "$@"
+./publish.sh cat "$@"
+./publish.sh mouse "$@"
+./publish.sh rat "$@"
+./publish_human.sh female "$@"
+./publish_human.sh male "$@"
